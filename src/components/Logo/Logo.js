8dd13fe28afe2045e978b223cdab6a889db37fc5
Mariana Souza <marianacaldassouza@gmail.com>
2019-07-21 18:17:53 -0700

    starting repository

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,23 @@
+import React from 'react' ;
+import Tilt from 'react-tilt';
+import  './Logo.css';
+import logo from './img/logo.png'
+
+const Logo = () => {
+
+	return(
+	<div className='ma4 mt0'>
+		<Tilt className="Tilt br2 shadow-2" options={{ max : 60 }} style={{ height: 150, width: 150 }} >
+	 		<div className="Tilt-inner pa3">
+	 			<img src={logo}  alt= "Our Face ID logo" />
+
+
+	 		</div>
+		</Tilt>
+	</div>
+
+	);
+
+}
+
+export default Logo;
