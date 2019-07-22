a7560ac2b01cfb3222be30c57aa0e0166796a565
Mariana Souza <marianacaldassouza@gmail.com>
2019-07-21 20:05:39 -0700

    Adding Imaage Link Form component

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,17 @@
+import React from 'react' ;
+
+const Rank = () => {
+
+	return(
+		<div>
+			<p className='f3'>
+				{'This app will detect faces in your pictures'}
+			</p>
+			
+		</div>
+
+	);
+
+}
+
+export default Rank;
