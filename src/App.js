100364e3f28562823948df3e27c4c2b519a14ddd
Mariana Souza <marianacaldassouza@gmail.com>
2019-07-22 23:05:32 -0700

    adding events and the clarifai API

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
@@ -7,7 +8,34 @@ import particlesOptions from './particlesOptions';
 import Particles from 'react-particles-js'; 
 import './App.css';
 
+const app = new Clarifai.App({
+ apiKey: '4f4a1abac428428d8b0071315b762030'
+});
+
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');
+    app.models.predict("4f4a1abac428428d8b0071315b762030", "https://samples.clarifai.com/face-det.jpg").then(
+    function(response) {
+      // do something with response
+    },
+    function(err) {
+      // there was an error
+    }
+  );
+  }
+
   render(){
     return (
       <div className="App">
@@ -16,7 +44,7 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
         {/*<FaceRecognition />*/}
       </div>
     );
