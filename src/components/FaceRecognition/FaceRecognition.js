016fb0dbbafa8c69c347e395ac8f7aa81ca279c4
Mariana Souza <marianacaldassouza@gmail.com>
2019-07-28 23:43:55 -0700

    adjusting imageUrl size on FaceReconition component

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,9 +1,10 @@
 import React from 'react' ;
+import './FaceRecognition.css'
 
 const FaceRecognition = ({ imageUrl }) => {
 
 	return(
-		<div className='center'>
+		<div className='center image-url'>
 			<img alt='' src={imageUrl} />
 		</div>
 
