1928f510b581918c90309400b342b8f88bfe8163
Mariana Souza <marianacaldassouza@gmail.com>
2019-08-24 22:10:31 -0700

    adding function displayFaceBox

--- src/App.js
+++ src/App.js
@@ -28,13 +28,17 @@ class App extends Component {
     const image = document.getElementById('inputimage');
     const width = Number(image.width);
     const height = Number(image.height);
-    return{
+    return {
       leftCol: clarifaiFace.left_col * width,
       topRow: clarifaiFace.top_row * height,
       rightCol: width - (clarifaiFace.right_col * width),
       bottomRow: height - (clarifaiFace.bottom_row * height)
-
     }
+  }
+
+  displayFaceBox = (box) =>{
+    console.log(box);
+    this.setState({box: box});
   }
 
   onInputChange = (event) => {
@@ -43,9 +47,12 @@ class App extends Component {
 
   onButtonSubmit = () => {
       this.setState({imageUrl: this.state.input});
-      app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-      .then(response =>this.calculateFaceLocation(response))
-      .catch(err=> console.log(err));
+      app.models
+        .predict(
+          Clarifai.FACE_DETECT_MODEL, 
+          this.state.input)
+        .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+        .catch( err => console.log(err));
   }
 
   render(){
