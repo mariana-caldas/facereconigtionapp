cb4917d4de9ade0aa498367c87c042ba412bc320
Mariana Souza <marianacaldassouza@gmail.com>
2020-01-19 19:02:42 -0800

    Fixing signin and signout navigation

--- src/App.js
+++ src/App.js
@@ -22,7 +22,8 @@ class App extends Component {
       input: '',
       imageUrl: '',
       box:{},
-      route: 'signin'
+      route: 'signin',
+      isSignedIn: false
     }
   }
 
@@ -59,6 +60,11 @@ class App extends Component {
   }
 
   onRouteChange = (route) => {
+    if(route === 'signout'){
+      this.setState({isSignedIn: false})
+    } else if (route === 'home') {
+      this.setState({isSignedIn: true})
+    }
     this.setState({route: route});
   }
 
@@ -67,7 +73,7 @@ class App extends Component {
       <div className="App">
         <Particles className ='particles'
         params={particlesOptions} />
-        <Navigation onRouteChange={this.onRouteChange} />
+        <Navigation isSignedIn={this.state.isSignedIn} onRouteChange={this.onRouteChange} />
         { this.state.route === 'home'
          ? <div>
             <Logo />
